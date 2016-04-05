4254186bad3ee115481ad49a8e3b2a00dda4f124
zenghui <zhzenghui@gmail.com>
2016-04-05 09:59:47 +0800

    标注变量出处

--- PriviteClassUDID/PriviteClassUDID/UIDevice+IOKit_Extensions.m
+++ PriviteClassUDID/PriviteClassUDID/UIDevice+IOKit_Extensions.m
@@ -25,17 +25,35 @@
     void *IOKit = dlopen("/System/Library/Frameworks/IOKit.framework/IOKit", RTLD_NOW);
     if (IOKit)
     {
+//        IOKitLib.h    L:103
         mach_port_t *kIOMasterPortDefault = dlsym(IOKit, "kIOMasterPortDefault");
+        
+        
+//      L: 76
         CFMutableDictionaryRef (*IOServiceMatching)(const char *name) = dlsym(IOKit, "IOServiceMatching");
+        
+        
+        
+        
+//      L:385
         mach_port_t (*IOServiceGetMatchingService)(mach_port_t masterPort, CFDictionaryRef matching) = dlsym(IOKit, "IOServiceGetMatchingService");
+       
+//      L1084
         CFTypeRef (*IORegistryEntryCreateCFProperty)(mach_port_t entry, CFStringRef key, CFAllocatorRef allocator, uint32_t options) = dlsym(IOKit, "IORegistryEntryCreateCFProperty");
+        
+//      L: 221
         kern_return_t (*IOObjectRelease)(mach_port_t object) = dlsym(IOKit, "IOObjectRelease");
+        
         
         if (kIOMasterPortDefault && IOServiceGetMatchingService && IORegistryEntryCreateCFProperty && IOObjectRelease)
         {
+            
+//            
             mach_port_t platformExpertDevice = IOServiceGetMatchingService(*kIOMasterPortDefault, IOServiceMatching("IOPlatformExpertDevice"));
             if (platformExpertDevice)
             {
+//              IOKitKeys L:150
+//              IOKit.bridgesupport  L:1332
                 CFTypeRef platformSerialNumber = IORegistryEntryCreateCFProperty(platformExpertDevice, CFSTR("IOPlatformSerialNumber"), kCFAllocatorDefault, 0);
                 if (platformSerialNumber && CFGetTypeID(platformSerialNumber) == CFStringGetTypeID())
                 {
