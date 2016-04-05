93b933697bfb76014ed0e0b512b1e10c2c8b226d
zenghui <zhzenghui@gmail.com>
2016-04-05 09:49:01 +0800

    序列号获取

---
+++ PriviteClassUDID/PriviteClassUDID/UIDevice+IOKit_Extensions.h
@@ -0,0 +1,16 @@
+//
+//  UIDevice+IOKit_Extensions.h
+//  PriviteClassUDID
+//
+//  Created by xy on 16/4/5.
+//  Copyright © 2016年 haro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIDevice(IOKit_Extensions)
+
+- (NSString *) serialNumber;
+
+
+@end
